05dc092d69a675c99fd8e8684a65faa4cdff455d
j-tirelli <john.tirelli@gmail.com>
2020-10-25 19:43:41 -0700

    Added configuration files for loaderio

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const HOST = 'localhost';
 
 app.use(express.static(__dirname));
 
+app.get('/loaderio-ef5fd1d1ea935ab4dda1f3cfaf6f910a', async (req, res) => {
+  res.status(200).send('loaderio-ef5fd1d1ea935ab4dda1f3cfaf6f910a');
+});
+
+app.get('/loaderio/payload.json', async (req, res) => {
+  res.status(200).json(payload);
+});
+
 app.get('/info', (req, res, next) => {
   res.send('This is a proxy service which proxies to the 4 services present in the Team 2 FEC project.');
 });
